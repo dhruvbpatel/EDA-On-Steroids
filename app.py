c6e97a9beabd681672d5153a767609af76405093
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2020-06-19 17:14:52 +0530

    add features in eda module

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ def main():
     # st.title("EDA on the GO")
     st.sidebar.title("Select Task")
     
+
+    
     html_header="""
     <div style ="background-color:tomato;padding:10px">
     <h2 style="color:white;text-align:center;">EDA-On-Steroids</h2>
@@ -27,6 +29,7 @@ def main():
 
     data = st.file_uploader("Upload Data here (CSV file only)",type=['csv'])
     
+
 
     ## EDA 
     if choice=='EDA':
@@ -43,7 +46,7 @@ def main():
                     st.subheader("RAW DATA")    
                     st.write(df)
 
-            if st.checkbox("Select Columns",False):
+            if st.checkbox("View Multiple Columns",False):
                 select_column = st.multiselect("Select Columns",col_list)
                 col_df = df[select_column]
                 st.dataframe(col_df)
@@ -79,14 +82,30 @@ def main():
                 num_col_count = len(df.columns)-cat_col_count
                 num_cat_df = pd.DataFrame({"Categorical Column Count":[cat_col_count],"Numerical Column Count":[num_col_count]},index=["Counts"])
                 st.write(num_cat_df)
+            
+### individual column summary
+            if st.checkbox("Show Individual Column Summary",False):
+                col_options = df.columns.to_list()
+                selected_col=st.selectbox("Select Column",col_options)
 
-                
+                st.write("Statistics for ",selected_col)
+                st.write(df[selected_col].describe())
 
+                st.write("Number of Distinct Values in ",selected_col, ":")
+                unique_val = df[selected_col].nunique()
+                st.write(unique_val)
 
+                st.write("Unique values percentage :")
+                unique_val_percent = 100*unique_val/len(df)
+                st.write(unique_val_percent,"%")
 
+                st.write("Missing Values :")
+                missing_val_col = df[selected_col].isnull().sum()
+                st.write(missing_val_col)
 
-
-
+                st.write("Missing Values percentage :")
+                missing_val_col_percent = 100*missing_val_col/len(df)
+                st.write(missing_val_col_percent,"%")
 
                 
             # st.selectbox("columns",df.columns.to_list())
@@ -113,8 +132,46 @@ def main():
 
 
     if choice=='Train Model':
-        st.subheader("Build the model")    
+        #st.subheader("Build the model")  
+        st.subheader("Module will be added soon !!!!")  
+    
+    footer = """
+    
 
+
+    <footer style="margin-top:280px;">
+    Made with 🧡 by <a href="https://github.com/dhruvbpatel">@dhruvhimself</a>
+    <br><br>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <a href="https://www.linkedin.com/in/dhruv-patel-1057/" target="_blank">
+    <img src="https://img.icons8.com/metro/24/000000/linkedin.png"/>
+    </a>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <a href="https://github.com/dhruvbpatel" target="_blank">
+    <img src="https://img.icons8.com/material-sharp/26/000000/github.png"/>
+    </a>
+    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+    <a href="https://twitter.com/dhruvhimself" target="_blank">
+    <img src="https://img.icons8.com/metro/26/000000/twitter.png"/>
+    </a>
+    </footer>
+
+    """
+    st.sidebar.markdown(footer, unsafe_allow_html=True)
+    
+    hide_streamlit_style = """
+            <style>
+                        #MainMenu {visibility: hidden;}
+
+
+                        
+
+            </style>
+
+            """
+
+
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
     
 if __name__ == '__main__':
     main()

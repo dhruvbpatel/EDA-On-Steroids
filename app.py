19977f3fdf46a100122f1ea1d7361d25590692b8
Dhruv patel <33346480+dhruvbpatel@users.noreply.github.com>
2020-06-20 17:18:43 +0530

    update UI

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 ## pckgs
 import streamlit as st
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt
 import matplotlib
 import numpy as np
 import sklearn
@@ -8,17 +8,14 @@ import pandas as pd
 import seaborn as sns
 
 
-
 ## main func
 
 def main():
     # st.title("EDA on the GO")
-    
-    st.sidebar.title("Select Task")
-    
 
-    
-    html_header="""
+    st.sidebar.title("Select Task")
+
+    html_header = """
     <head>
     <title>EDA</title>
     </head>
@@ -27,37 +24,34 @@ def main():
     
     </div>
     """
-    st.markdown(html_header,unsafe_allow_html=True)
+    st.markdown(html_header, unsafe_allow_html=True)
 
-    option =["EDA","Data Visualization","Train Model"]
-    choice=st.sidebar.selectbox("Select Task",option)
+    option = ["EDA", "Data Visualization", "Train Model"]
+    choice = st.sidebar.selectbox("Select Task", option)
     st.subheader("Exploratory Data Analysis")
 
-    data = st.file_uploader("Upload Data here (CSV file only)",type=['csv'],encoding='cp1252')
-    
+    data = st.file_uploader("Upload Data here (CSV file only)", type=[
+                            'csv'], encoding='cp1252')
 
-
-    ## EDA 
-    if choice=='EDA':
-        
+    ## EDA
+    if choice == 'EDA':
 
         if data is not None:
-            df=pd.read_csv(data)
+            df = pd.read_csv(data)
             col_list = df.columns.to_list()
 
-
             if st.checkbox("Show raw data", False):
-                
+
                 if data is not None:
-                    st.subheader("RAW DATA")    
+                    st.subheader("RAW DATA")
                     st.write(df)
 
-            if st.checkbox("View Multiple Columns",False):
-                select_column = st.multiselect("Select Columns",col_list)
+            if st.checkbox("View Multiple Columns", False):
+                select_column = st.multiselect("Select Columns", col_list)
                 col_df = df[select_column]
                 st.dataframe(col_df)
-            
-            if st.checkbox("Show Dataset Statistics",False):
+
+            if st.checkbox("Show Dataset Statistics", False):
                 st.write("Stats")
                 st.write(df.describe())
 
@@ -72,52 +66,53 @@ def main():
 
                 missing_val = df.isnull().sum()
                 missing_val_df = pd.DataFrame(missing_val, columns=["Counts"])
-                st.write("Missing Values Chart")   ## missing value chart
-                
+                st.write("Missing Values Chart")  # missing value chart
+
                 st.bar_chart(missing_val_df)
                 st.write("Number of Missing Values")
-                
+
                 st.write(missing_val_df)
 
                 st.write("Missing Cells Percentage")
-                
+
                 missing_val_sum = missing_val.sum()
-                missing_val_percent = 100*(missing_val_sum)/(df.shape[0]*df.shape[1])
-                st.write(missing_val_percent,"%")
+                missing_val_percent = 100 * \
+                    (missing_val_sum)/(df.shape[0]*df.shape[1])
+                st.write(missing_val_percent, "%")
 
                 st.write("Column Category")
-                cat_col = [col for col in df.columns.to_list() if df[col].dtype=='object']
+                cat_col = [col for col in df.columns.to_list()
+                           if df[col].dtype == 'object']
 
                 cat_col_count = len(cat_col)
                 num_col_count = len(df.columns)-cat_col_count
-                num_cat_df = pd.DataFrame({"Categorical Column Count":[cat_col_count],"Numerical Column Count":[num_col_count]},index=["Counts"])
+                num_cat_df = pd.DataFrame({"Categorical Column Count": [
+                                          cat_col_count], "Numerical Column Count": [num_col_count]}, index=["Counts"])
                 st.write(num_cat_df)
-            
+
 ### individual column summary
-            if st.checkbox("Show Individual Column Summary",False):
+            if st.checkbox("Show Individual Column Summary", False):
                 col_options = df.columns.to_list()
-                selected_col=st.selectbox("Select Column",col_options)
+                selected_col = st.selectbox("Select Column", col_options)
 
-                if df[selected_col].dtype=='O':
+                if df[selected_col].dtype == 'O':
 
-                    ct_plot = sns.countplot(x=selected_col,data=df)
+                    ct_plot = sns.countplot(x=selected_col, data=df)
                     st.pyplot()
                 else:
-                    dst_plot=sns.distplot(df[selected_col],kde=True)
+                    dst_plot = sns.distplot(df[selected_col], kde=True)
                     st.pyplot()
-                    
 
-
-                st.write("Statistics for ",selected_col)
+                st.write("Statistics for ", selected_col)
                 st.write(df[selected_col].describe())
 
-                st.write("Number of Distinct Values in ",selected_col, ":")
+                st.write("Number of Distinct Values in ", selected_col, ":")
                 unique_val = df[selected_col].nunique()
                 st.write(unique_val)
 
                 st.write("Unique values percentage :")
                 unique_val_percent = 100*unique_val/len(df)
-                st.write(unique_val_percent,"%")
+                st.write(unique_val_percent, "%")
 
                 st.write("Missing Values :")
                 missing_val_col = df[selected_col].isnull().sum()
@@ -125,71 +120,86 @@ def main():
 
                 st.write("Missing Values percentage :")
                 missing_val_col_percent = 100*missing_val_col/len(df)
-                st.write(missing_val_col_percent,"%")
+                st.write(missing_val_col_percent, "%")
 
-                
             # st.selectbox("columns",df.columns.to_list())
-            
+
     if choice == 'Data Visualization':
         df = pd.read_csv(data)
         col_list = df.columns.to_list()
         st.subheader("Data Visualization")
         corr_option = ["Show Correlation between all Features",
-                        "Show Correlation between among Selected Features"]
+                       "Show Correlation between among Selected Features"]
 
-        corr_choice=st.selectbox("Select Correlation Plot",corr_option)        
+        corr_choice = st.selectbox("Select Correlation Plot", corr_option)
 
         if corr_choice == "Show Correlation between all Features":
-            st.write(sns.heatmap(df.corr(),annot=True))
+            st.write(sns.heatmap(df.corr(), annot=True))
             st.pyplot()
         if corr_choice == "Show Correlation between among Selected Features":
-            num_col = [col for col in df.columns.to_list() if df[col].dtype != 'object']  ## selecting only numerical cols
-            select_col=st.multiselect("Select columns",num_col)
+            # selecting only numerical cols
+            num_col = [col for col in df.columns.to_list()
+                       if df[col].dtype != 'object']
+            select_col = st.multiselect("Select columns", num_col)
             select_col_df = df[select_col]
 
-            st.write(sns.heatmap(select_col_df.corr(),annot=True))
+            st.write(sns.heatmap(select_col_df.corr(), annot=True))
             st.pyplot()
             st.write("More features to be added soon !!!")
 
-    if choice=='Train Model':
-        #st.subheader("Build the model")  
-        st.subheader("Module will be added soon !!!!")  
-    
+    if choice == 'Train Model':
+        #st.subheader("Build the model")
+        st.subheader("Module will be added soon !!!!")
+
     footer = """
     
 
 
-    <p style="margin-top:280px;">
-    Made with 🧡 by <a href="https://github.com/dhruvbpatel">@dhruvhimself</a>
-    <br><br>
-&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    <a href="https://www.linkedin.com/in/dhruv-patel-1057/" target="_blank">
-    <img src="https://img.icons8.com/metro/24/000000/linkedin.png"/>
-    </a>
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    <a href="https://github.com/dhruvbpatel" target="_blank">
-    <img src="https://img.icons8.com/material-sharp/26/000000/github.png"/>
-    </a>
-    &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-    <a href="https://twitter.com/dhruvhimself" target="_blank">
-    <img src="https://img.icons8.com/metro/26/000000/twitter.png"/>
-    </a>
-    </p>
+        <p style="margin-top:280px;">
+        Made with 🧡 by <a href="https://github.com/dhruvbpatel">@dhruvhimself</a>
+        <br><br>
+    &emsp;
+        <a href="https://www.linkedin.com/in/dhruv-patel-1057/" target="_blank">
+        <img src="https://img.icons8.com/metro/24/000000/linkedin.png"/>
+        </a>
+        &emsp;&emsp;&emsp;&emsp;
+        <a href="https://github.com/dhruvbpatel" target="_blank">
+        <img src="https://img.icons8.com/material-sharp/26/000000/github.png"/>
+        </a>
+        &emsp;&emsp;&emsp;&emsp;
+        <a href="https://twitter.com/dhruvhimself" target="_blank">
+        <img src="https://img.icons8.com/metro/26/000000/twitter.png"/>
+        </a>
+        </p>
 
-    """
-    st.sidebar.markdown(footer, unsafe_allow_html=True)
+<style>
+
+.footer { 
+    position: relative; 
     
+    height:[footer-height] 
+}
+</style>
+
+  
+  
+
+
+
+        """
+    st.sidebar.markdown(footer, unsafe_allow_html=True)
+
     hide_streamlit_style = """
             <style>
                         #MainMenu {visibility: hidden;}
-                        footer{visibility: hidden;}
+                        
                         title{visibility: hidden;}
             </style>
 
             """
 
-
     st.markdown(hide_streamlit_style, unsafe_allow_html=True)
-    
+
+
 if __name__ == '__main__':
     main()
